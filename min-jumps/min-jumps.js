92cc72ad00955a7913e37d2069bd9fe067a4b742
mtoffl01 <tofflerm@gmail.com>
2020-02-19 16:37:14 -0500

    passing all min jump specs

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -2,7 +2,22 @@
 
 // Complete this algo
 const minJumps = arr => {
+  if (arr.length === 0){
+    return -1;
+  }
 
+  let memo = [0];
+  for(let i=1; i<arr.length; i++){
+    memo[i] = Number.MAX_SAFE_INTEGER;
+  }
+  for(let i=0; i<arr.length; i++){
+    for(let j=i; j<=(i+arr[i]); j++){
+      if(j<arr.length){
+        memo[j] = Math.min( memo[j], (memo[i]+1) );
+      }
+    }
+  }
+  return memo[arr.length-1];
 };
 
 module.exports = minJumps
